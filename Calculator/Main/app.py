57dfd0f43989f4575edd292e7cc7d8e74e3f0c26
Adrian Moldovan <85124099+AdrianMold@users.noreply.github.com>
2024-01-29 16:32:21 +0100

    Delete Calculator directory

--- Calculator/Main/app.py
+++
@@ -1,8 +0,0 @@
-from Calculator.MiniCalculator import mini_calculator   #importarea clasei
-
-if __name__ == '__main__':
-    #intializare obiect
-    operatie_1 = mini_calculator()
-    #apelare metoda
-    operatie_1.setare(7)
-    operatie_1.adunare(3)
